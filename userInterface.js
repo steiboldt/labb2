656fc8fb7722ae590982829a28a536e5ab568e54
Marcus Steiboldt <ms227cn@student.lnu.se>
2022-11-02 12:16:36 +0100

    restructure and dividing methods

--- userInterface.js
+++ userInterface.js
@@ -7,7 +7,10 @@ export class userInterface {
     
   }
 
-
+startApp() {
+  this.startingPage()
+  this.startingPageNav()
+}
 /**
  * Starting page for the console interface.
  */
@@ -18,30 +21,47 @@ export class userInterface {
     console.log('(1) From English to code')
     console.log('(2) From code to English')
     console.log('(0) Exit Application')
+  }
 
+  startingPageNav() {
     const prompt = promptSync()
     const selection = prompt('Your selection: ')
 
     if (selection === '1') {
       this.stringToTranslate()
     } else if (selection === '2') {
-        this.codeToEnglishpage()
+        this.runCodeToEnglishPage()
     } else if (selection === '0') {
       return
+    } else if (selection !== '1' || '2'  || '0') {
+      throw new Exception('Error: Invalid input')
     }
   }
 
   /**
-   * the code to english page.
+   * Run ui and navigation for code to english page.
    */
-  codeToEnglishpage() {
+  runCodeToEnglishPage() {
+    this.codeToEnglishPage()
+    this.codeToEnglishPageNav()
+  }
+
+  /**
+   * the page for translating english into codelanguages.
+   */
+  codeToEnglishPage() {
     console.log('')
     console.log('What codelanguage would you like to translate from?')
     console.log('(1) Morse')
     console.log('(2) Binary')
     console.log('(3) Caesarcipher (Rotation 13)')
     console.log('(0) Return to startpage')
+  }
 
+  /**
+   * Navigating the codeToEnglishPage
+   */
+  codeToEnglishPageNav() {
     const prompt = promptSync()
     const selection = prompt('Your selection: ')
 
@@ -56,9 +76,14 @@ export class userInterface {
       this.keepTranslatingCode()
     } else if (selection === '0') {
       this.startingPage()
+    } else if (selection !== '1' || '2' || '3' || '0') {
+      throw new Error('Error: Invalid input')
     }
   }
 
+  /**
+   * Translate from Morse method.
+   */
   fromMorse() {
     console.log('')
     console.log('To translate from morse use symbols . or - ')
@@ -72,6 +97,9 @@ export class userInterface {
     translator.morseToEnglish(morseText)
   }
 
+  /**
+   * Translate from binary method.
+   */
   fromBinary() {
     console.log('')
     console.log('To translate from morse use 0 or 1 ')
@@ -83,6 +111,21 @@ export class userInterface {
 
     const translator = new Translator() 
     console.log(translator.binaryToEnglish(binaryText))
+  }
+
+  /**
+   * Translate from caesar cipher 13 method.
+   */
+  fromCaesar() {
+    console.log('')
+    console.log('To translate from Caesarcipher use A-Z ')
+   
+
+    const prompt = promptSync()
+    const caesarText = prompt('Your text: ')
+
+    const translator = new Translator() 
+    console.log(translator.englishToCaesar13(caesarText))
   }
 
   /**
@@ -104,13 +147,21 @@ export class userInterface {
    * @param {String} stringToTranslate 
    */
   englishToCodePage(stringToTranslate) {
+    
     console.log('')
     console.log('What codelanguage would you like to translate to?')
     console.log('(1) Morse')
     console.log('(2) Binary')
     console.log('(3) Caesarcipher (Rotation 13)')
     console.log('(0) Return to startpage')
+    
+    this.englishToCodeNav(stringToTranslate)
+    
 
+    
+  }
+
+  englishToCodeNav(stringToTranslate) {
     const prompt = promptSync()
     const selection = prompt('Your selection: ')
 
@@ -125,10 +176,14 @@ export class userInterface {
       this.keepTranslatingEnglish(stringToTranslate)
     } else if (selection === '0') {
       this.startingPage()
+    } else if (selection !== '1' || '2' || '3' || '0') {
+      throw new Error('Error: Invalid input')
     }
-    
   }
 
+  /**
+   * Prompt for continuing to translate code.
+   */
   keepTranslatingCode() {
     console.log('')
     console.log('Would you like to keep translating?')
@@ -137,9 +192,9 @@ export class userInterface {
     const selection = prompt('y/n?: ')
 
     if (selection === 'y') {
-      this.codeToEnglishpage()
+      this.runCodeToEnglishPage()
     } else {
-      this.startingPage()
+      this.startApp()
     }
   }
 
